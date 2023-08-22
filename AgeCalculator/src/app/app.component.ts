cf2454f3ad8f071ea428015c67148bf1bb2ebcd8
Shahrzad JavadiKushesh <Sh.Javadi@persianswitch.net>
2023-08-22 15:12:33 +0330

    jalali date

--- AgeCalculator/src/app/app.component.ts
+++ AgeCalculator/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { ValidatorFn } from '@angular/forms';
-import { FormBuilder, FormGroup, AbstractControl, ValidationErrors, Validators, FormControl, FormArray } from '@angular/forms';
+import { FormBuilder, FormGroup, AbstractControl, ValidationErrors, Validators } from '@angular/forms';
+import * as moment from 'jalali-moment';
 
 @Component({
   selector: 'app-root',
@@ -29,47 +30,39 @@ export class AppComponent {
   }
 
   calculateAge() {
-    let today = new Date();
+    let jalaliDate = moment().locale('fa').format('YYYY/M/D');
 
-    console.log(today)
-
-    console.log(today.getDate())
-    console.log(today.getMonth())
-    console.log(today.getFullYear())
+    console.log("jalali date:", jalaliDate);
 
     let year = this.inputForm.value.year;
     let month = this.inputForm.value.month;
     let date = this.inputForm.value.date;
 
-    if (year===null || year === undefined || month === null || month === undefined || date === null || date === undefined){
+    if (year === null || year === undefined || month === null || month === undefined || date === null || date === undefined) {
       return;
     }
-
-    console.log(typeof (year))
-    console.log(typeof (month))
-    console.log(typeof (date))
 
     console.log(year)
     console.log(month)
     console.log(date)
 
-    if (year && +year > +today.getFullYear()) {
-      this.ageMessage = 'Invalid year';
+    if (year && +year > +jalaliDate.substring(0,4)) {
+      this.ageMessage = `Year can't be more than ${jalaliDate.substring(0,4)}`;
       return;
     }
     if (month && (+month < 1 || +month > 12)) {
       this.ageMessage = 'Invalid month';
       return;
     }
-    if (date && month && (+date < 1 || (+date > 30 && +month > 6 && +month < 12) || (+month === 12 && +!date > 29))) {
+    if (date && month && (+date < 1 || (+date > 30 && +month > 6 && +month < 12) || (+month == 12 && +date > 29))) {
       this.ageMessage = 'Invalid date';
       return;
     }
 
     // Calculate the age in years, months, and days
-    let ageYears = today.getFullYear() - +year;
-    let ageMonths = today.getMonth() - +month + 1;
-    let ageDays = today.getDate() - +date;
+    let ageYears = +jalaliDate.substring(0, 4) - +year;
+    let ageMonths = +jalaliDate.substring(5,6) - +month + 1;
+    let ageDays = +jalaliDate.substring(7) - +date;
     console.log("years: ", ageYears)
     console.log("months: ", ageMonths)
     console.log("days: ", ageDays)
@@ -80,14 +73,16 @@ export class AppComponent {
     }
     if (ageDays < 0) {
       ageMonths--;
-      let daysInMonth = new Date(+!year, +!month, 0).getDate();
+      let daysInMonth = new Date(+year, +month, 0).getDate();
       ageDays += daysInMonth;
     }
 
     this.ageMessage = `You are ${ageYears} years, ${ageMonths} months, and ${ageDays} days old`;
-    
+
   }
 
 }
+
+
 
 
