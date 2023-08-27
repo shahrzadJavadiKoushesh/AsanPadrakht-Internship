d466b77426b59673e054d0d81e56f1e8c76c5a66
Shahrzad JavadiKushesh <Sh.Javadi@persianswitch.net>
2023-08-27 11:46:13 +0330

    add add-comment component html and scss

--- InteractiveCommentSection/src/app/app.module.ts
+++ InteractiveCommentSection/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddCommentComponent } from './add-comment/add-comment.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    AddCommentComponent,
   ],
   imports: [
     BrowserModule,
