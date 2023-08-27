92d247bbb818216af74cec9ac3b7962ece5672a7
Shahrzad JavadiKushesh <Sh.Javadi@persianswitch.net>
2023-08-27 11:31:13 +0330

    remove services

--- InteractiveCommentSection/src/app/app.module.ts
+++ InteractiveCommentSection/src/app/app.module.ts
@@ -3,20 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CommentListComponent } from './comment-list/comment-list.component';
-import { CommentFormComponent } from './comment-form/comment-form.component';
-import { ReplyFormComponent } from './reply-form/reply-form.component';
-import { CommentComponent } from './comment/comment.component';
-import { ReplyComponent } from './reply/reply.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CommentListComponent,
-    CommentFormComponent,
-    ReplyFormComponent,
-    CommentComponent,
-    ReplyComponent
   ],
   imports: [
     BrowserModule,
