4b5c4bfbdb345116064fe08f05063dae723c6ea3
shahrzad <shahrzadjkoushesh@gmail.com>
2023-09-10 21:20:25 +0330

    add methods for not found

--- observableInteractiveCommentSection/src/app/comment.service.ts
+++ observableInteractiveCommentSection/src/app/comment.service.ts
@@ -183,4 +183,13 @@ export class CommentService {
     }
     this.commentsSubject.next(lastComments);
   }
+
+  // updateNotFoundVisible() {
+  //   this.notFoundVisible = !!this.searchText;
+  // }
+
+  // setSearchText(searchText: string) {
+  //   this.searchText = searchText;
+  //   this.updateNotFoundVisible();
+  // }
 }
