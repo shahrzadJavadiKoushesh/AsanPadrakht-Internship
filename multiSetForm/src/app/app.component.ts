5fa08cbc6c50e22f13a33501ee6d180d6bab185e
Shahrzad JavadiKushesh <Sh.Javadi@persianswitch.net>
2023-09-10 16:27:15 +0330

    add first step

--- multiSetForm/src/app/app.component.ts
+++ multiSetForm/src/app/app.component.ts
@@ -1,4 +1,12 @@
 import { Component } from '@angular/core';
+import { FormArray, FormGroup } from '@angular/forms';
+import { FormlyFormOptions, FormlyFieldConfig } from '@ngx-formly/core';
+
+
+export interface StepType {
+  label: string;
+  fields: FormlyFieldConfig[];
+}
 
 @Component({
   selector: 'app-root',
@@ -7,4 +15,40 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'multiSetForm';
+
+  activedStep = 0;
+
+  steps: StepType[] = [
+    {
+      label: 'Personal info',
+      fields: [
+        {
+          key: 'Name',
+          type: 'input',
+          templateOptions: {
+            label: 'name',
+            required: true,
+          },
+        },
+        {
+          key: 'email',
+          type: 'input',
+          templateOptions: {
+            type: 'email',
+            label: 'email',
+            required: true,
+          },
+        },
+        {
+          key: 'Phone Number',
+          type: 'input',
+          templateOptions: {
+            type: 'tel',
+            label: 'phone',
+            required: true,
+          },
+        },
+      ],
+    },
+  ];
 }
