314e83fde3994d87998150dfce76bcda8029a373
Shahrzad JavadiKushesh <Sh.Javadi@persianswitch.net>
2023-09-19 14:43:26 +0330

    add formService

--- dynamicForms/src/app/app.component.ts
+++ dynamicForms/src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit  } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'dynamicForms';
+
+  dynamicForm!: FormGroup;
+
+  constructor(private fb: FormBuilder){}
+
+  ngOnInit(): void {
+    this.dynamicForm = this.fb.group({
+
+    })
+  }
 }
